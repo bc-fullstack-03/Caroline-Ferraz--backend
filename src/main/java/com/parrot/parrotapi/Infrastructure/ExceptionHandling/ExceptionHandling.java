5e431527badf52f02975bdada721bb8595c793fb
Caroline Ferraz <projetos.crrz@gmail.com>
2023-05-06 17:52:57 -0300

    Feat: Adicionando tratamento de excessão para erros de validação de formulário

--- src/main/java/com/parrot/parrotapi/Infrastructure/ExceptionHandling/ExceptionHandling.java
+++ src/main/java/com/parrot/parrotapi/Infrastructure/ExceptionHandling/ExceptionHandling.java
@@ -2,6 +2,8 @@ package com.parrot.parrotapi.Infrastructure.ExceptionHandling;
 
 import com.amazonaws.services.glue.model.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,7 +11,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionHandling {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity EntityNotFoundHandler(){
+    public ResponseEntity entityNotFoundHandler(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity validationErrorHandler(MethodArgumentNotValidException ex){
+        var fieldErrors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(fieldErrors.stream().map(FieldErrorsResponse::new).toList());
+    }
+
+    private record FieldErrorsResponse(String field, String message){
+        public FieldErrorsResponse(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
