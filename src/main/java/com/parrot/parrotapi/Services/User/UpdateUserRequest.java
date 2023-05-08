6e2eb70861387e9dd5ed1b7dcada4e4771bd23c9
Caroline Ferraz <projetos.crrz@gmail.com>
2023-05-08 13:38:19 -0300

    Fix: Adicionando verificação no método update de User, para verificar se o e-mail passado na requisição já existe no banco de dados

--- src/main/java/com/parrot/parrotapi/Services/User/UpdateUserRequest.java
+++ src/main/java/com/parrot/parrotapi/Services/User/UpdateUserRequest.java
@@ -1,24 +1,18 @@
 package com.parrot.parrotapi.Services.User;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.data.mongodb.core.index.Indexed;
 
-import java.util.UUID;
 
 @Data
 public class UpdateUserRequest {
 
-    @NotNull
-    private UUID id;
-    private String name;
-    private String photo;
+    public String name;
     @Email(message = "Este campo não contém um formato de e-mail válido")
     @Indexed(unique = true)
-    private String email;
+    public String email;
     @Size(min = 8, max=25, message = "A Senha deve ter no mínimo 8 e no máximo 25 caracteres")
-    private String password;
-
+    public String password;
 }
--- src/main/java/com/parrot/parrotapi/Services/User/UserService.java
+++ src/main/java/com/parrot/parrotapi/Services/User/UserService.java
@@ -1,10 +1,8 @@
 package com.parrot.parrotapi.Services.User;
 
-import com.parrot.parrotapi.Domain.Post;
 import com.parrot.parrotapi.Infrastructure.IUserRepository;
 import com.parrot.parrotapi.Domain.User;
 import com.parrot.parrotapi.Services.FileUpload.IFileUploadService;
-import com.parrot.parrotapi.Services.Post.IPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,9 +19,6 @@ public class UserService implements IUserService {
 
     @Autowired
     private IUserRepository _userRepository;
-
-    @Autowired
-    private IPostService _postService;
 
     @Autowired
     private PasswordEncoder _passwordEncoder;
@@ -45,30 +40,35 @@ public class UserService implements IUserService {
         }
     }
 
-    public Page<GetUsersResponse> getUsers(Pageable pageable) {
+    public Page<GetUsersResponse> getAllUsers(Pageable pageable) {
         var response = _userRepository.findAll(pageable).map(GetUsersResponse::new);
         return response;
     }
 
     public void updateUser(UpdateUserRequest request) {
-        var optionalUser = _userRepository.findById(request.getId());
-        User user = optionalUser.orElseThrow(() -> new NoSuchElementException("Usuário não encontrado"));
-        user.updateUserData(request);
-        _userRepository.save(user);
+        var user = this.getUserBySecurityContextHolder();
+        if(_userRepository.findUserByEmail(request.email) != null){
+            throw new RuntimeException("Este Usuário já existe.");
+        } else {
+            user.updateUserData(request);
+            _userRepository.save(user);
+        }
     }
 
     public void deleteUser(UUID id){
-        _userRepository.deleteById(id);
+//        var user = this.getUserBySecurityContextHolder();
+//        if(user.getId() != id){
+//            throw new RuntimeException("Operação inválida.");
+//        } else {
+            _userRepository.deleteById(id);
+//        }
+
     }
 
     public GetUserByIdResponse getUserById(UUID id){
         var optionalUser = _userRepository.findById(id);
         User user = optionalUser.orElseThrow(() -> new NoSuchElementException("Usuário não encontrado"));
         return new GetUserByIdResponse(user.getId(), user.getName(), user.getPhoto(), user.getFollowing(), user.getFollowers());
-    }
-
-    public Page<Post> getPostsByUser(UUID userId, Pageable pageable){
-        return _postService.getPostsByUser(userId, pageable);
     }
 
     public void followOrUnfollowUser(UUID userId, FollowOrUnfollowUserRequest request){
@@ -85,29 +85,28 @@ public class UserService implements IUserService {
         _userRepository.save(userFollower);
     }
 
-//    public FindUserResponse findUserByEmail(String email){
-//        var user = _userRepository.findUserByEmail(email);
-//        var response = new FindUserResponse(user.getId(), user.getName(), user.getPhoto(), user.getEmail());
-//        return response;
-//    }
-
     public User getUser(String email){
         return _userRepository.findUserByEmail(email);
     }
 
     public void uploadPhotoProfile(MultipartFile photoFile) throws Exception {
-        var id = ((GetUserByIdResponse) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        var user = this.getUserBySecurityContextHolder();
         var photo = "";
         try{
-            var fileName = id + "." + photoFile.getOriginalFilename().substring(photoFile.getOriginalFilename().lastIndexOf(".") + 1);
+            var fileName = user.getId() + "." + photoFile.getOriginalFilename().substring(photoFile.getOriginalFilename().lastIndexOf(".") + 1);
             photo = _fileUploadService.upload(photoFile, fileName);
 
         } catch (Exception e){
             throw new Exception(e.getMessage());
         }
-        var optionalUser = _userRepository.findById(id);
-        User user = optionalUser.orElseThrow(() -> new NoSuchElementException("Usuário não encontrado"));
         user.setPhoto(photo);
         _userRepository.save(user);
+    }
+
+    public User getUserBySecurityContextHolder(){
+        var id = ((GetUserByIdResponse) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        var optionalUser = _userRepository.findById(id);
+        User user = optionalUser.orElseThrow(() -> new NoSuchElementException("Usuário não encontrado"));
+        return user;
     }
 }
