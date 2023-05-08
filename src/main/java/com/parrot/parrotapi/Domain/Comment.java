620d95b2dd4d6576ec306c3c17a8b13f9292ebd0
Caroline Ferraz <projetos.crrz@gmail.com>
2023-05-08 19:51:40 -0300

    Chore: Organizando identação e removendo comentários e um método que acabou não sendo utilizado

--- src/main/java/com/parrot/parrotapi/Domain/Comment.java
+++ src/main/java/com/parrot/parrotapi/Domain/Comment.java
@@ -1,21 +1,15 @@
 package com.parrot.parrotapi.Domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Data
-//@Document
 public class Comment {
 
     @Id
     private UUID id;
-    //@DBRef(lazy = true)
-    //@JsonBackReference
     private UUID postId;
     private UUID userId; // user's id that've made the comment
     private String text;
@@ -28,11 +22,4 @@ public class Comment {
     }
 
     protected void setId() { this.id = UUID.randomUUID(); }
-
-    public void updateCommentData(Comment data){
-        if(data.getText() != null){
-            this.text = data.getText();
-        }
-    }
-
 }
--- src/main/java/com/parrot/parrotapi/Services/Post/UpdatePostRequest.java
+++ src/main/java/com/parrot/parrotapi/Services/Post/UpdatePostRequest.java
@@ -1,11 +1,9 @@
 package com.parrot.parrotapi.Services.Post;
 
-import com.parrot.parrotapi.Domain.Comment;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import java.util.List;
 import java.util.UUID;
 
 @Data
